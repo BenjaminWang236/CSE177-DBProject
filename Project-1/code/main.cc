91c9a1ef94cdc00f6b79be230ea5c79c171d5699
brianlam37 <brianlam37@gmail.com>
2019-03-05 04:50:37 -0800

    Separated Project 2 from 1

--- Project-1/code/main.cc
+++ Project-1/code/main.cc
@@ -1,68 +1,208 @@
-#include <iostream>
+#include <vector>
 #include <string>
+#include <iostream>
 
+#include "Schema.h"
 #include "Catalog.h"
-extern "C"{
-	#include "QueryParser.h"
-}
-#include "QueryOptimizer.h"
-#include "QueryCompiler.h"
-#include "RelOp.h"
 
 using namespace std;
 
+void menu(Catalog& c){
+	int choice;
 
-// these data structures hold the result of the parsing
-extern struct FuncOperator* finalFunction; // the aggregate function
-extern struct TableList* tables; // the list of tables in the query
-extern struct AndList* predicate; // the predicate in WHERE
-extern struct NameList* groupingAtts; // grouping attributes
-extern struct NameList* attsToSelect; // the attributes in SELECT
-extern int distinctAtts; // 1 if there is a DISTINCT in a non-aggregate query
+	cout << "==============================" << endl;
+	cout << "\t1: Create table" << endl;
+	cout << "\t2: Drop table" << endl;
+	cout << "\t3: Display Database Content" << endl;
+	cout << "\t4: Save content" << endl;
+	cout << "\t5: Set number of distincts of an attribute" << endl;
+	cout << "\t6: Set number of tuples of a table" << endl;
+	cout << "\t7: Set Datafile of a table" << endl;
+	cout << "\t8: Get number of distincts for an attribute" << endl;
+	cout << "\t9: Get number of tuples from a table" << endl;
+	cout << "\t10: Get Datafile of a table" << endl;
+	cout << "\t11: Exit" << endl;
+	cout << "==============================" << endl;
+	cout << "Your Choice: ";
+	cin >> choice;
+	switch(choice){
 
-extern "C" int yyparse();
-extern "C" int yylex_destroy();
+		case 1:
+		{
+			string tName;
+			cout << "Table name: ";
+			cin >> tName;
+			cout << "Enter number of attributes: ";
+			int numA;
+			cin >> numA;
+			vector<string> aN;
+			vector<string> aT;
+			for (int i = 0; i < numA;i++){
+				cout << "Enter attribute name: ";
+				string tN;
+				cin >>tN;
+				aN.push_back(tN);
+				cout << "Enter attribute type: ";
+				string tT;
+				cin >> tT;
+				aT.push_back(tT);
+			}
+			c.CreateTable(tName,aN,aT);
+			menu(c);
+		}
+			break;
+		case 2:
+		{
+			string tName;
+			cout << "Enter table to drop: ";
+			cin >> tName;
+			c.DropTable(tName);
+			menu(c);
+		}
+			break;
+		case 3:
+		{
+			cout << c << endl;
+			menu(c);
+		}
+			break;
+		case 4:
+		{
+			c.Save();
+			menu(c);
+		}
+			break;
+		case 5:
+		{
+			cout << "Enter table name: ";
+			string tName;
+			cin >> tName;
+			cout << "Enter attribute name: ";
+			string aName;
+			cin >> aName;
+			cout << "Enter number of distincts: ";
+			unsigned int tD;
+			cin >> tD;
+			c.SetNoDistinct(tName,aName,tD);
+			menu(c);
+		}
+			break;
+		case 6:
+		{
+			cout << "Enter table name: ";
+			string tName;
+			cin >> tName;
+			cout << "Enter number of tuples: ";
+			unsigned int nTuple;
+			cin >> nTuple;
+			c.SetNoTuples(tName,nTuple);
+			menu(c);			
+		}
+			break;
+		case 7:
+		{
+			cout << "Enter table name: ";
+			string tName;
+			cin >> tName;
+			cout << "Enter File name ";
+			string fName;
+			cin >> fName;
+			c.SetDataFile(tName,fName);
+			menu(c);			
+		}
+			break;
+		case 8:
+		{
+			cout << "Enter table name: ";
+			string tName;
+			cin >> tName;
+			cout << "Enter attribute name: ";
+			string aName;
+			cin >> aName;
+			unsigned int tD;
+			c.GetNoDistinct(tName,aName,tD);
+			cout << "Number of distincts is: " << tD << endl;
+			menu(c);			
+		}
+			break;
+		case 9:
+		{
+			cout << "Enter table name: ";
+			string tName;
+			cin >> tName;
+			unsigned int nTuple;
+			c.GetNoTuples(tName,nTuple);
+			cout << "Number of tuples is: " << nTuple << endl;
+			menu(c);
+		}			
+			break;
+		case 10:
+		{
+			cout << "Enter table name: ";
+			string tName;
+			cin >> tName;
+			string fName;
+			c.GetDataFile(tName,fName);
+			cout << "Datafile is: " << fName << endl;
+			menu(c);				
+		}
+			break;
+		case 11:{
+			cout << "Closing menu" << endl;
+		}
+	}
 
-
+}
 
 int main () {
-	// this is the catalog
-	string dbFile = "catalog.sqlite";
-	Catalog catalog(dbFile);
+	// string table = "region", attribute, type;
+	// vector<string> attributes, types;
+	// vector<unsigned int> distincts;
 
-	// this is the query optimizer
-	// it is not invoked directly but rather passed to the query compiler
-	QueryOptimizer optimizer(catalog);
+	// attribute = "r_regionkey"; attributes.push_back(attribute);
+	// type = "INTEGER"; types.push_back(type);
+	// distincts.push_back(5);
+	// attribute = "r_name"; attributes.push_back(attribute);
+	// type = "STRING"; types.push_back(type);
+	// distincts.push_back(5);
+	// attribute = "r_comment"; attributes.push_back(attribute);
+	// type = "STRING"; types.push_back(type);
+	// distincts.push_back(5);
 
-	// this is the query compiler
-	// it includes the catalog and the query optimizer
-	QueryCompiler compiler(catalog, optimizer);
+	// Schema s(attributes, types, distincts);
+	// Schema s1(s), s2; s2 = s1;
+
+	// string a1 = "r_regionkey", b1 = "regionkey";
+	// string a2 = "r_name", b2 = "name";
+	// string a3 = "r_commen", b3 = "comment";
+
+	// s1.RenameAtt(a1, b1);
+	// s1.RenameAtt(a2, b2);
+	// s1.RenameAtt(a3, b3);
+
+	// s2.Append(s1);
+
+	// vector<int> keep;
+	// keep.push_back(5);
+	// keep.push_back(0);
+	// s2.Project(keep);
+
+	// cout << s << endl;
+	// cout << s1 << endl;
+	// cout << s2 << endl;
 
 
-	// the query parser is accessed directly through yyparse
-	// this populates the extern data structures
-	int parse = -1;
-	if (yyparse () == 0) {
-		cout << "OK!" << endl;
-		parse = 0;
-	}
-	else {
-		cout << "Error: Query is not correct!" << endl;
-		parse = -1;
-	}
+	 string dbFile = "catalog.sqlite";
+	// Catalog c(dbFile);
 
-	yylex_destroy();
+	// c.CreateTable(table, attributes, types);
+	// string filepath = "urmomma";
+	// c.SetDataFile(table,filepath);
+	// unsigned int sigh = 6;
+	// c.SetNoTuples(table,sigh);
+	// cout << c << endl;
 
-	if (parse != 0) return -1;
-
-	// at this point we have the parse tree in the ParseTree data structures
-	// we are ready to invoke the query compiler with the given query
-	// the result is the execution tree built from the parse tree and optimized
-	QueryExecutionTree queryTree;
-	compiler.Compile(tables, attsToSelect, finalFunction, predicate,
-		groupingAtts, distinctAtts, queryTree);
-
-	cout << queryTree << endl;
-
+	Catalog c(dbFile);
+	menu(c);
 	return 0;
 }
